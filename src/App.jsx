2c0d79b4bfab170352f44b97ef73107814b46bb2
FingNaresh <nareshyadavansi3565@gmail.com>
2025-07-29 14:47:03 +0530

    Add routing and authentication components; remove unused CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import './App.css'
+
+import Body from './components/Body'
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-      Namaste Everyone, let's build a Movie App using React and TMDB API
-      <h1>Movie App</h1>
-      <h2>Using TMDB API</h2>
-      <h3>React</h3>
-      <h4>TMDB API</h4>
-    </div>
+    <Body />
   )
 }
 
