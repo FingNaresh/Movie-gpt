02cfa33095030c622d941fae3973502676eae9c3
FingNaresh <nareshyadavansi3565@gmail.com>
2025-11-28 21:58:39 +0530

    Refactor authentication handling and add constants for logo and user avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { removeUser } from "../utils/userSlice";
+import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = useSelector(store=>store.user);
+  const user = useSelector((store) => store.user);
+  console.log("Redux User Data:", user);
   const dispatch = useDispatch();
 
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-        // Remove user from Redux
-        dispatch(removeUser());
-
-        // Navigate to login
-        navigate("/");
-      })
+      .then(() => {})
       .catch((err) => {
         console.log(err);
         navigate("/error");
       });
   };
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid,
+            email,
+            displayName,
+            photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    // unsubscribe when component unmounts
+    return ()=> unsubscribe();
+  }, []);
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
-      {/* Logo */}
       <img
         className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="logo"
       />
 
       {user && (
-      <div className="flex items-center gap-3">
-        <img
-          className="w-10 h-10 rounded"
-          alt="usericon"
-          // src="https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp"
-          src={user?.photURL}
-        />
+        <div className="flex items-center gap-3">
+          <img
+            className="w-10 h-10 rounded"
+            alt="usericon"
+            src={user?.photoURL}
+          />
 
-        <button onClick={handleSignOut} className="text-white font-semibold">
-          Sign Out
-        </button>
-      </div>
+          <button onClick={handleSignOut} className="text-white font-semibold">
+            Sign Out
+          </button>
+        </div>
       )}
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,28 +7,26 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
-  const dispatch= useDispatch();
+
+  const dispatch = useDispatch();
 
   const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
   const handleButtonClick = () => {
-    // Validate input
     const message = checkValidData(email.current.value, password.current.value);
 
     setErrorMessage(message);
     if (message) return;
 
-    // SIGN UP LOGIC
     if (!isSignInForm) {
       createUserWithEmailAndPassword(
         auth,
@@ -37,22 +35,23 @@ const Login = () => {
       )
         .then((userCredential) => {
           const user = userCredential.user;
-           updateProfile(user, {
+
+          return updateProfile(user, {
             displayName: name.current.value,
-            photoURL:
-              "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp",
-          }).then(() => {
-             const { uid, email, displayName, photoURL } = auth.currentUser;
-                    dispatch(
-                      addUser({
-                        uid: uid,
-                        email: email,
-                        displayName: displayName,
-                        photoURL: photoURL,
-                      })
-                    );
-            navigate("/browse");
+            photoURL: { USER_AVATAR },
           });
+        })
+        .then(() => {
+          const { uid, email, displayName, photoURL } = auth.currentUser;
+
+          dispatch(
+            addUser({
+              uid,
+              email,
+              displayName,
+              photoURL,
+            })
+          );
         })
         .catch((error) => {
           setErrorMessage(error.code + " - " + error.message);
@@ -64,7 +63,7 @@ const Login = () => {
         password.current.value
       )
         .then((userCredential) => {
-          navigate("/browse");
+          const user = userCredential.user;
         })
         .catch((error) => {
           setErrorMessage(error.code + " - " + error.message);
